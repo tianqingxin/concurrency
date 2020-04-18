16a1658a597ff71c98b503ed213487758094a0da
田庆新 <18766170465@163.com>
2020-04-18 23:23:16 +0800

    22

--- src/main/java/com/inspur/concurrency/controller/TestController.java
+++ src/main/java/com/inspur/concurrency/controller/TestController.java
@@ -1,11 +1,9 @@
 package com.inspur.concurrency.controller;
 
-import com.inspur.concurrency.singleton.SingleTonExample6;
 import com.inspur.concurrency.utils.EntityUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
