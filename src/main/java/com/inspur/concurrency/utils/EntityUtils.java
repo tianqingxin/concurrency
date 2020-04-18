4051055b864b94514ea0079ab7199e0c65954606
田庆新 <18766170465@163.com>
2020-04-18 23:16:57 +0800

    22

--- src/main/java/com/inspur/concurrency/utils/EntityUtils.java
+++ src/main/java/com/inspur/concurrency/utils/EntityUtils.java
@@ -3,8 +3,6 @@ package com.inspur.concurrency.utils;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 /**
